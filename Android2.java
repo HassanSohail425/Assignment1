116d23ba5b43e7ce84c74af34e1c46a7745edc4c
HassanSohail425 <52451631+HassanSohail425@users.noreply.github.com>
2019-09-04 23:07:03 +0500

    Add files via upload

---
+++ Android2.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package android2;
+
+/**
+ *
+ * @author hassa
+ */
+public class Android2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static class Student{
+ void openRegistrationPage(){
+     try{
+ long coursesOfferred[] = new long[1000000000];
+ System.out.println("Print");
+     }
+     catch(Exception e)
+     {
+         System.out.println("Registration period active");
+     }
+     System.out.println("Outof try catch");
+     
+     }
+
+ }
+    
+
+    public static void android2(String[] args) {
+        // TODO code application logic here
+        Student currentStudent= new Student();
+ currentStudent.openRegistrationPage();
+    }   
+ 
+}
+
